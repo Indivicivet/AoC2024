0292b0d26a4edb2748069fe8210bfa0c6b7133a8
indi <indivicivet@gmail.com>
2024-12-01 19:34:17 +0000

    simple day01 part 2

--- day01.py
+++ day01.py
@@ -1,5 +1,10 @@
+import collections
 from pathlib import Path
 
 txt = (Path(__file__).parent / "inputs" / "day01.txt").read_text()
 
-print(sum(abs(int(b) - int(a)) for a, b in zip(*map(sorted, zip(*map(str.split, txt.splitlines()))))))
+l1, l2 = zip(*map(str.split, txt.splitlines()))
+print(sum(abs(int(b) - int(a)) for a, b in zip(sorted(l1), sorted(l2))))
+
+d2 = collections.Counter(l2)
+print(sum(int(a) * d2[a] for a in l1))
