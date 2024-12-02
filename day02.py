9fa888237e81da9e78a8e2356bb14dc779e81eb0
indi <indivicivet@gmail.com>
2024-12-02 23:51:39 +0000

    simplify code - its ok to just have one set

--- day02.py
+++ day02.py
@@ -1,14 +1,14 @@
-from collections import defaultdict
 import itertools
 from pathlib import Path
 
 txt = (Path(__file__).parent / "inputs" / "day02.txt").read_text()
 
 # generate all possible values assuming min = 0 and ignoring reversals
-safe_by_len = defaultdict(set)
+safe = set()
 for steps in range(1, 9):  # max len; 9 is enough here
     for vals in itertools.product(*[[1, 2, 3]] * steps):
-        safe_by_len[steps + 1].add(tuple(itertools.accumulate((0, ) + vals)))
+        safe.add(tuple(itertools.accumulate((0, ) + vals)))
+
 
 reports = [
     [int(x) for x in line.split()]
@@ -20,16 +20,15 @@ reports_normed = [
 ]
 
 part1 = sum(
-    report in safe_by_len[len(report)] or report[::-1] in safe_by_len[len(report)]
+    report in safe or report[::-1] in safe
     for report in reports_normed
 )
 print(part1)
 
 part2 = sum(
-    report in safe_by_len[len(report)]
-    or any(
-        report[:i] + report[i+1:] in safe_by_len[len(report) - 1]
-        for i in range(len(report))
+    any(
+        report[:i] + report[i+1:] in safe
+        for i in range(len(report) + 1)
     )
     for base_report in reports_normed
     for report in [base_report, base_report[::-1]]
