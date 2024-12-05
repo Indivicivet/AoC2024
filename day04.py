f464a1855371e529a97a0f2bd8cbb50395da4c8c
indi <indivicivet@gmail.com>
2024-12-05 21:35:58 +0000

    add wip part 2 solution (too low...)

--- day04.py
+++ day04.py
@@ -18,7 +18,7 @@ arr = np.array([
 ])
 print(arr)
 
-PHASE_RATE = np.pi / 4
+PHASE_RATE = np.pi / 2
 phased = np.exp(1j * PHASE_RATE * arr)
 template_raw = np.exp(-1j * PHASE_RATE * np.arange(4)[:, np.newaxis])
 
@@ -39,3 +39,16 @@ print(sum(
 
 # fourier = np.fft.fft2(phased)
 # Image.fromarray(10 * np.abs(fourier) ** 0.5).show()
+
+phased_pt2 = phased * (arr > 0)  # remove X's
+assert PHASE_RATE == np.pi / 2, "using hardcoded phase values for part 2 :)"
+template_pt2_raw = np.array([[1, 0, 1], [0, 1j, 0], [-1, 0, -1]])
+convs_pt2 = [
+    signal.convolve(phased_pt2, template[::traverse_ud, :])
+    for template in [template_pt2_raw, template_pt2_raw.T]
+    for traverse_ud in [-1, 1]
+]
+print(sum(
+    np.sum(np.abs(conv) > 5 - 0.01)
+    for conv in convs_pt2
+))
