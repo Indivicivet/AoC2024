d4a22dcbc3990cb2efe7e77a4806d15970187377
indi <indivicivet@gmail.com>
2024-12-05 21:51:08 +0000

    fix day 4 part 2 I hope

--- day04.py
+++ day04.py
@@ -41,14 +41,14 @@ print(sum(
 # Image.fromarray(10 * np.abs(fourier) ** 0.5).show()
 
 phased_pt2 = phased * (arr > 0)  # remove X's
-assert PHASE_RATE == np.pi / 2, "using hardcoded phase values for part 2 :)"
-template_pt2_raw = np.array([[1, 0, 1], [0, 1j, 0], [-1, 0, -1]])
+assert PHASE_RATE == np.pi / 4, "using hardcoded phase values for part 2 :)"
+template_pt2_raw = np.array([[1, 0, 1], [0, np.exp(1j * np.pi / 4), 0], [1j, 0, 1j]])
 convs_pt2 = [
-    signal.convolve(phased_pt2, template[::traverse_ud, :])
+    signal.convolve(phased_pt2, template[::traverse_sign, ::traverse_sign])
     for template in [template_pt2_raw, template_pt2_raw.T]
-    for traverse_ud in [-1, 1]
+    for traverse_sign in [-1, 1]
 ]
 print(sum(
-    np.sum(np.abs(conv) > 5 - 0.01)
+    np.sum(np.abs(conv) > 5 - 0.001)
     for conv in convs_pt2
 ))
