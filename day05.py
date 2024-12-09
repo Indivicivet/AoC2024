55d4810fdf95ac38b5cf279b562e8c6e1990e7d3
indi <indivicivet@gmail.com>
2024-12-09 23:52:36 +0000

    gen comprehension code; still doesn't work, I think I'm misunderstanding day5 X__X

--- day05.py
+++ day05.py
@@ -4,12 +4,9 @@ raw_page_rules, raw_updates = data.split("\n\n")
 
 page_order_graph = dict(row.split("|") for row in raw_page_rules.splitlines())
 updates = [row.split(",") for row in raw_updates.splitlines()]
-result = 0
-for update in updates:
-    update_ok = True
-    for i, v in enumerate(update):
-        if page_order_graph[v] in update[:i]:
-            update_ok = False
-    if update_ok:
-        result += int(update[len(update) // 2])
+result = sum(
+    int(update[len(update) // 2])
+    for update in updates
+    if all(page_order_graph[v] not in update[:i] for i, v in enumerate(update))
+)
 print(result)
