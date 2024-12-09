ebb08f6e9810ff7bc4f313fc59b91cb68ecf4afc
indi <indivicivet@gmail.com>
2024-12-09 23:06:32 +0000

    generalize day8 to part 2

--- day08.py
+++ day08.py
@@ -11,26 +11,25 @@ for j, row in enumerate(data.splitlines()):
 
 LEN = len(data.splitlines()[0])
 LENY = len(data.splitlines())
-total_antinodes = 0
-antinodes = defaultdict(lambda: np.array([LEN]*2))
-for key, positions in nodes.items():
-    for key2, positions2 in nodes.items():
-        if key != key2:
-            continue
-        if key == ".":
-            continue
-        for position in positions:
-            for position2 in positions2:
-                if position == position2:
-                    continue
-                offs_x = position2[1] - position[1]
-                offs_y = position2[0] - position[0]
-                for harm in [
-                    (position2[0] + offs_y, position2[1] + offs_x),
-                    (position[0] - offs_y, position[1] - offs_x),
-                ]:
-                    if harm[0] < 0 or harm[1] < 0 or harm[0] >= LENY or harm[1] >= LEN:
-                        continue
-                    antinodes[harm] = 1
-print(sum(np.sum(anvals) for anvals in antinodes.values()))
 
+for part2 in [False, True]:
+    antinodes = np.zeros([LENY, LEN], dtype=int)
+    for key, positions in nodes.items():
+        for key2, positions2 in nodes.items():
+            if key != key2:
+                continue
+            if key == ".":
+                continue
+            for position in positions:
+                for position2 in positions2:
+                    if position == position2:
+                        continue
+                    offs_x = position2[1] - position[1]
+                    offs_y = position2[0] - position[0]
+                    for offs_rel_pos in range(-LEN-1, LEN+1) if part2 else [-1, 2]:
+                        harm_x = position[0] + offs_y * offs_rel_pos
+                        harm_y = position[1] + offs_x * offs_rel_pos
+                        if harm_x < 0 or harm_y < 0 or harm_y >= LENY or harm_x >= LEN:
+                            continue
+                        antinodes[harm_y, harm_x] = 1
+    print(np.sum(antinodes))
