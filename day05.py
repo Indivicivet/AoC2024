b6920b33c5bb8ffa094b25ea98ee5efc29c01abe
indi <indivicivet@gmail.com>
2024-12-10 22:30:30 +0000

    fix day 5; can have multiple page orders per first page ^^; oops

--- day05.py
+++ day05.py
@@ -1,12 +1,21 @@
+from collections import defaultdict
+
 from aocd import data
 
 raw_page_rules, raw_updates = data.split("\n\n")
 
-page_order_graph = dict(row.split("|") for row in raw_page_rules.splitlines())
+page_order_graph = defaultdict(list)
+for row in raw_page_rules.splitlines():
+    pg1, pg2 = row.split("|")
+    page_order_graph[pg1].append(pg2)
 updates = [row.split(",") for row in raw_updates.splitlines()]
 result = sum(
     int(update[len(update) // 2])
     for update in updates
-    if all(page_order_graph[v] not in update[:i] for i, v in enumerate(update))
+    if all(
+        later_page not in update[:i]
+        for i, v in enumerate(update)
+        for later_page in page_order_graph[v]
+    )
 )
 print(result)
