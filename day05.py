fd39be3db9cabf4f41db3d314a1e9635a0f7783b
indi <indivicivet@gmail.com>
2024-12-10 22:38:08 +0000

    how not to do day 5 part 2

--- day05.py
+++ day05.py
@@ -1,4 +1,7 @@
 from collections import defaultdict
+import random
+
+from tqdm import tqdm
 
 from aocd import data
 
@@ -9,13 +12,26 @@ for row in raw_page_rules.splitlines():
     pg1, pg2 = row.split("|")
     page_order_graph[pg1].append(pg2)
 updates = [row.split(",") for row in raw_updates.splitlines()]
+
+
+def is_ok(upd):
+    return all(
+        later_page not in upd[:i]
+        for i, v in enumerate(upd)
+        for later_page in page_order_graph[v]
+    )
+
+
 result = sum(
     int(update[len(update) // 2])
     for update in updates
-    if all(
-        later_page not in update[:i]
-        for i, v in enumerate(update)
-        for later_page in page_order_graph[v]
-    )
+    if is_ok(update)
 )
 print(result)
+
+result_all_fixed = 0
+for update in tqdm(updates):
+    while not is_ok(update):
+        random.shuffle(update)  # this is insanely slow.
+    result_all_fixed += int(update[len(update) // 2])
+print(result_all_fixed - result)
