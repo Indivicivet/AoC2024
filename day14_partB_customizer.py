5242b9a823daf0aaf19057c23d96c75748d05350
indi <indivicivet@gmail.com>
2024-12-21 19:50:10 +0000

    rename day14_ to partB, ditto customizer, also permits just using data normally

--- day14_partB_customizer.py
+++ day14_partB_customizer.py
@@ -1,6 +1,6 @@
 import itertools
 
-from aocd import get_data
+from aocd import data  # used for velocity
 import numpy as np
 from PIL import Image
 
@@ -9,13 +9,12 @@ HEIGHT = 103
 
 
 def from_image_path(path, iter_n_times=1234):
-    data_original = get_data(year=2024, day=14)  # used for velocity
     image_data = np.array(Image.open(path).convert("L"))
     print("custom image! number of robots:", (image_data > 0).sum())
     assert image_data.shape == (HEIGHT, WIDTH), (
         f"{image_data.shape} != {(HEIGHT, WIDTH)}"
     )
-    txt_line_iter = itertools.cycle(data_original.splitlines())
+    txt_line_iter = itertools.cycle(data.splitlines())
     data_out_list = []
     for j in range(image_data.shape[0]):
         for i in range(image_data.shape[1]):
