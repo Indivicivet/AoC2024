11e8b267ad5593f58195acb3ab844908354859ff
indi <indivicivet@gmail.com>
2024-12-25 00:58:21 +0000

    convert to a list in the hope it'd magic up an answer but the result is exactly the same

--- day16.py
+++ day16.py
@@ -19,20 +19,26 @@ circle = [
     (-1, 0),  # west
     (0, -1),  # south
 ]
-graph = {
-    (i, j, di, dj): [  # rotations; two possible
-        ((i, j, di2, dj2), ROTATION_COST)
-        for di2, dj2 in circle
-        if abs(di) != abs(di2)  # only include perpendicular, and we're in C4
-    ]
-    + (  # move option - only one possibility
-        [((i + di, j + dj, di, dj), MOVE_COST)]
-        if (i + di, j + dj) in open_tiles
-        else []
+graph = [
+    (
+        (i, j, di, dj),
+        (i, j, di2, dj2),
+        ROTATION_COST,
     )
     for i, j in open_tiles
     for di, dj in circle
-}
+    for di2, dj2 in circle
+    if abs(di) != abs(di2)  # only include perpendicular, and we're in C4
+] + [
+    (
+        (i, j, di, dj),
+        (i + di, j + dj, di, dj),
+        MOVE_COST,
+    )
+    for i, j in open_tiles
+    for di, dj in circle
+    if (i + di, j + dj) in open_tiles
+]
 
 
 def make_1d(i, j, di, dj):  # obviously slightly silly :)
@@ -43,11 +49,10 @@ def make_1d(i, j, di, dj):  # obviously slightly silly :)
 pt0s = []
 pt1s = []
 weights = []
-for pt0, pt0_neighbours in graph.items():
-    for pt1, weight in pt0_neighbours:
-        pt0s.append(make_1d(*pt0))
-        pt1s.append(make_1d(*pt1))
-        weights.append(weight)
+for pt0, pt1, weight in graph:
+    pt0s.append(make_1d(*pt0))
+    pt1s.append(make_1d(*pt1))
+    weights.append(weight)
 sparse_mat = sparse.coo_matrix((weights, (pt0s, pt1s)))
 dijkstra_res = sparse.csgraph.dijkstra(
     sparse_mat,
